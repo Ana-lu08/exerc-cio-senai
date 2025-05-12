a96a42d7435f749a02dceba18fda4d0dfc8c307e
Ana-lu08 <analuizalemos083@gmail.com>
2025-05-12 15:48:28 -0300

    Add files via upload

---
+++ Media.py
@@ -0,0 +1,13 @@
+print("ola mundo")
+
+nome= input("Qual seu nome?")
+n1= float(input("Primeira nota: "))
+n2= float(input("Segunda nota: "))
+n3= float(input("Terceira nota: "))
+media= round((n1+n2+n3) /3, 2)
+print("Média: ", media)
+
+
+
+
+
---
+++ imc.py
@@ -0,0 +1,8 @@
+nome= input ("Digite seu nome:")
+altura= float(input ("Digite sua altura:"))
+peso= float(input ("Digite seu peso:"))
+imc= round(peso / (altura*altura), 2)
+
+print (imc)
+
+
